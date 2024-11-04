9607ddf08ebfa86a554000641fd24e3a39e6654b
Chiara <chiara_dimarco@hotmail.com>
2024-11-04 11:51:28 -0300

    feat: restructure project directory

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
-const authRoutes = require('./routes/authRoutes');
+const authRoutes = require('./src/routes/authRoutes');
 
 const app = express();
 
