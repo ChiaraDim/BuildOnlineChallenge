e6cea2e7fa4d2cd575b06f0c58852cc46a24beff
Chiara <chiara_dimarco@hotmail.com>
2024-11-04 10:29:42 -0300

    fix(server): update default port

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const Contact = require('./models/contact');
 sequelize.sync({ force: false })
     .then(() => {
         console.log('Database synchronized.');
-        const PORT = process.env.PORT || 3000;
+        const PORT = process.env.PORT || 5000;
         app.listen(PORT, () => {
-            console.log('Server running at http://localhost:${PORT}');
+            console.log(`Server is running on http://localhost:${PORT}`);
         });
     })
     .catch(err => {
