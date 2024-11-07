380e8a66ba32487b4fa856f4b7b9ef46dfdfb614
Chiara <chiara_dimarco@hotmail.com>
2024-11-07 14:49:08 -0300

    feat: enhance login functionality

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,24 +1,25 @@
-import jwt from 'jsonwebtoken';
-import User from '../models/user';
 import { Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import UserRepository from '../repositories/userRepository';
 
-export const login = async (req: Request, res: Response) => {
+export const login = async (req: Request, res: Response): Promise<void> => {
+  try {
     const { email, password } = req.body;
+    const user = await UserRepository.findByEmail(email);
 
-    try{
-        const user = await User.findOne({ where: { email } });
-
-        if (!user || user.password !== password) {
-            res.status(401).json({ message: 'Invalid credentials' });
-        }
-
-        if (!process.env.JWT_SECRET) {
-            throw new Error('JWT secret is not defined');
-        }
-
-        const token = jwt.sign({ email: user!.email }, process.env.JWT_SECRET, { expiresIn: '1h' });
-        res.json({ token: token });
-    } catch (error) {
-        res.status(500).json({ message: 'Internal server error', error });
+    if (!user || !bcrypt.compareSync(password, user.password)) {
+      res.status(401).json({ message: 'Invalid credentials' });
+      return;
     }
+
+    if (!process.env.JWT_SECRET) {
+      throw new Error('JWT secret is not defined');
+    }
+
+    const token = jwt.sign({ email: user.email }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.json({ token });
+  } catch (error) {
+    res.status(500).json({ message: 'Internal server error', error });
+  }
 };
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,40 +1,34 @@
-import { DataTypes, Model, Optional } from 'sequelize';
-import sequelize from '../config/database';
+import { DataTypes, Model } from 'sequelize';
+import db from '../config/database';
 
-interface UserAttributes {
-    id?: number;
-    email: string;
-    password: string;
-    createdAt?: Date;
-    updatedAt?: Date;
-}
-
-interface UserCreationAttributes extends Optional<UserAttributes, 'id' | 'createdAt' | 'updatedAt'> {}
-
-class User extends Model<UserAttributes, UserCreationAttributes> implements UserAttributes {
-    public id!: number;
-    public email!: string;
-    public password!: string;
-    public readonly createdAt!: Date;
-    public readonly updatedAt!: Date;
+export class User extends Model {
+  public id!: number;
+  public email!: string;
+  public password!: string;
 }
 
 User.init(
-    {
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
+  {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      autoIncrement: true,
+      primaryKey: true,
     },
-    {
-        sequelize,
-        modelName: 'User',
-    }
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize: db,
+    modelName: 'User',
+    tableName: 'users',
+  }
 );
 
 export default User;
